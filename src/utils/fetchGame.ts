41721035a656567b29c49deb50da535f0fea2314
samuel Dos Santos Gonçalves da Silva <samuelsantos20021@gmail.com>
2025-03-11 20:20:48 -0300

    feat: request data from localstorage:3000

--- src/utils/fetchGame.ts
+++ src/utils/fetchGame.ts
@@ -1,14 +1,12 @@
 const fetchGame = async (date: string): Promise<string> => {
-  const savedData = JSON.parse(localStorage.getItem(`game-${date}`) || '{}');
+  const savedData = JSON.parse(localStorage.getItem(`game-${date}`) || "{}");
 
   if (savedData.word) return savedData.word;
 
   // Request API here
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("PARALELEPIPEDO");
-    }, 100);
-  });
+  const response = await fetch(`http://localhost:3000/api/game/${date}`);
+  const data = await response.json();
+  return data.word.toUpperCase();
 };
 
 export default fetchGame;
